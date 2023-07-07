3fc51a9351c7e035e56f33c3a28febea20736259
kkeirstead <kkeirstead@microsoft.com>
2023-07-07 11:40:40 -0700

    Tweaks.

--- index.js
+++ index.js
@@ -11,9 +11,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 
-// switch these to sets instead of arrays
-var modifiedFiles = []; // output
-var manuallyReview = []; // output
+function UpdateModifiedFiles(path)
+{
+  modifiedFiles.push(path);
+  core.setOutput('modifiedFiles', modifiedFiles.join("\n"));
+}
+
+function UpdateManuallyReview(path)
+{
+  manuallyReview.push(path);
+  core.setOutput('manuallyReview', manuallyReview.join("\n"));
+}
 
 const main = async () => {
 
@@ -28,6 +36,8 @@ const main = async () => {
     const mergePathPrefix = "merge/";
     const headPathPrefix = "head/";
 
+    var modifiedFiles = []; // output
+    var manuallyReview = []; // output
     
     var linksToCheck = [];
     var pathsToCheck = [];
@@ -43,198 +53,170 @@ const main = async () => {
     }
 
     // Scan each file in the learningPaths directory
-    //const readFileAsync = () => {
-      return new Promise((resolve) => {
-        fs.readdir(learningPathsDirectory, (err, files) => {
-          files.forEach(learningPathFile => {
+    fs.readdir(learningPathsDirectory, (err, files) => {
+      files.forEach(learningPathFile => {
 
-            fs.readFile(learningPathsDirectory + "/" + learningPathFile, (err, learningPathFileContent) => {
-              if (err) throw err;
+        fs.readFile(learningPathsDirectory + "/" + learningPathFile, (err, learningPathFileContent) => {
+          if (err) throw err;
 
-              var learningPathFileContentStr = learningPathFileContent.toString();
+          var learningPathFileContentStr = learningPathFileContent.toString();
 
-              var indices = [];
-              for(var pos = learningPathFileContentStr.indexOf(repoURLToSearch); pos !== -1; pos = learningPathFileContentStr.indexOf(repoURLToSearch, pos + 1)) {
-                  indices.push(pos);
-              }
+          var indices = [];
+          for(var pos = learningPathFileContentStr.indexOf(repoURLToSearch); pos !== -1; pos = learningPathFileContentStr.indexOf(repoURLToSearch, pos + 1)) {
+              indices.push(pos);
+          }
 
-              // for each index, find next instance of ')' to get the end of the link
-              for(var i = 0; i < indices.length; i++)
+          // for each index, find next instance of ')' to get the end of the link
+          for(var i = 0; i < indices.length; i++)
+          {
+            var index = indices[i];
+            var endIndex = learningPathFileContentStr.indexOf(')', index);
+            var link = learningPathFileContentStr.substring(index, endIndex);
+            linksToCheck.push(link);
+            //console.log("Link: " + link);
+
+            const linePrefix = "#L";
+
+            const indexOfLineNumber = link.indexOf("#L");
+            const hasLineNumber = indexOfLineNumber !== -1;
+
+            var lineNumber = "";
+
+            if (hasLineNumber)
+            {
+              lineNumber = link.substring(indexOfLineNumber + linePrefix.length, link.length);
+            }
+
+            const pathStartIndex = link.indexOf("src");
+            const pathEndIndex = hasLineNumber ? indexOfLineNumber : endIndex;
+
+            const trimmedFilePath = link.substring(pathStartIndex, pathEndIndex);
+
+            const pathIndex = pathsToCheck.indexOf(trimmedFilePath)
+            console.log(trimmedFilePath);
+            console.log(pathIndex);
+            console.log("-----");
+
+            if (pathIndex !== -1)
+            {
+              UpdateModifiedFiles(trimmedFilePath);
+
+              if (hasLineNumber)
               {
-                var index = indices[i];
-                var endIndex = learningPathFileContentStr.indexOf(')', index);
-                var link = learningPathFileContentStr.substring(index, endIndex);
-                linksToCheck.push(link);
-                //console.log("Link: " + link);
+                console.log("Has Line Number: " + lineNumber);
 
-                const linePrefix = "#L";
+                var newContentLines = [];
+                var existingContentLines = [];
 
-                const indexOfLineNumber = link.indexOf("#L");
-                const hasLineNumber = indexOfLineNumber !== -1;
-
-                var lineNumber = "";
-
-                if (hasLineNumber)
-                {
-                  lineNumber = link.substring(indexOfLineNumber + linePrefix.length, link.length);
-                }
-
-                const pathStartIndex = link.indexOf("src");
-                const pathEndIndex = hasLineNumber ? indexOfLineNumber : endIndex;
-
-                const trimmedFilePath = link.substring(pathStartIndex, pathEndIndex);
-
-                const pathIndex = pathsToCheck.indexOf(trimmedFilePath)
-                console.log(trimmedFilePath);
-                console.log(pathIndex);
-                console.log("-----");
-
-                if (pathIndex !== -1)
-                {
-                  modifiedFiles.push(trimmedFilePath); // push everything that's modified onto this list
-
-                  console.log("MF: " + modifiedFiles);
-
-                  if (hasLineNumber)
+                // not sure the best way to do this...might have to count newlines on merge and head
+                // and then compare the lines.
+                fs.readFile(mergePathPrefix + pathsToCheck[pathIndex], (err, newContent) => { // can probably also used trimmedFilePath here...depends how prefixes work
+                  if (err || learningPathFileContentStr === null || learningPathFileContentStr.length === 0)
                   {
-                    console.log("Has Line Number: " + lineNumber);
-
-                    var newContentLines = [];
-                    var existingContentLines = [];
-
-                    // not sure the best way to do this...might have to count newlines on merge and head
-                    // and then compare the lines.
-                    fs.readFile(mergePathPrefix + pathsToCheck[pathIndex], (err, newContent) => { // can probably also used trimmedFilePath here...depends how prefixes work
-                      if (err || learningPathFileContentStr === null || learningPathFileContentStr.length === 0)
-                      {
-                        // file no longer exists
-                        // recommend that the link be manually reviewed
-                        manuallyReview.push(trimmedFilePath)
-                        console.log("MR_LN: " + manuallyReview);
-                      }
-                      else
-                      {
-                        var newContentStr = newContent.toString();
-
-                        // file does exist, check line numbers
-                        newContentLines = newContentStr.split("\n");
-
-                        fs.readFile(headPathPrefix + pathsToCheck[pathIndex], (err, existingContent) => {
-                        
-                          if (err || existingContent === null || existingContent.length === 0)
-                          {
-                            console.log("This should never happen: " + err);
-                            // this should never happen
-                          }
-                          else
-                          {
-                            console.log("Else: " + existingContent.toString());
-
-                            var existingContentStr = existingContent.toString();
-
-                            existingContentLines = existingContentStr.split("\n");
-
-                            if (existingContentLines.length >= lineNumber && newContentLines.length >= lineNumber)
-                            {
-                              const lineNumberInt = Number(lineNumber) - 1;
-                              console.log("Existing: " + existingContentLines[lineNumberInt].trim());
-                              console.log("New: " + newContentLines[lineNumberInt].trim());
-                              const equalLines = existingContentLines[lineNumberInt].trim() === newContentLines[lineNumberInt].trim()
-
-                              if (!equalLines)
-                              {
-                                const updatedLineNumber = newContentLines.indexOf(existingContentLines[lineNumberInt]) + 1;
-
-                                if (updatedLineNumber !== -1)
-                                {
-                                  console.log("Updated line number: " + updatedLineNumber);
-
-                                  var updatedLearningPathFileContent = learningPathFileContentStr.substring(0, endIndex - lineNumber.length) + updatedLineNumber + learningPathFileContentStr.substring(endIndex, learningPathFileContentStr.length);
-
-                                  console.log("Before" + learningPathFileContentStr);
-                                  console.log("After" + updatedLearningPathFileContent);
-
-                                  fs.writeFile(learningPathsDirectory + "/" + learningPathFile, updatedLearningPathFileContent, (err) => {
-                                    if (err)
-                                    {
-                                      console.log("Failed to write: " + err);
-                                    }
-              
-                                  });
-
-                                }
-                                else
-                                {
-                                  // recommend that the link be manually reviewed
-                                  manuallyReview.push(trimmedFilePath)
-                                }
-                              }
-                            }
-
-                          }
-                        });
-
-                      }
-                    });
-
+                    UpdateManuallyReview(trimmedFilePath);
                   }
                   else
                   {
-                    // just check that the file still exists
-                    fs.readFile(mergePathPrefix + pathsToCheck[pathIndex], (err, content) => {
-                      if (err || content === null || content.length === 0)
+                    var newContentStr = newContent.toString();
+
+                    // file does exist, check line numbers
+                    newContentLines = newContentStr.split("\n");
+
+                    fs.readFile(headPathPrefix + pathsToCheck[pathIndex], (err, existingContent) => {
+                    
+                      if (err || existingContent === null || existingContent.length === 0)
                       {
-                        // file no longer exists
-                        // recommend that the link be manually reviewed
-                        manuallyReview.push(trimmedFilePath)
-                        console.log("MR: " + manuallyReview);
+                        console.log("This should never happen: " + err);
+                        // this should never happen
                       }
                       else
                       {
-                        console.log("Content that we read: " + content.toString());
+                        console.log("Else: " + existingContent.toString());
+
+                        var existingContentStr = existingContent.toString();
+
+                        existingContentLines = existingContentStr.split("\n");
+
+                        if (existingContentLines.length >= lineNumber && newContentLines.length >= lineNumber)
+                        {
+                          const lineNumberInt = Number(lineNumber) - 1;
+                          console.log("Existing: " + existingContentLines[lineNumberInt].trim());
+                          console.log("New: " + newContentLines[lineNumberInt].trim());
+                          const equalLines = existingContentLines[lineNumberInt].trim() === newContentLines[lineNumberInt].trim()
+
+                          if (!equalLines)
+                          {
+                            const updatedLineNumber = newContentLines.indexOf(existingContentLines[lineNumberInt]) + 1;
+
+                            if (updatedLineNumber !== -1)
+                            {
+                              console.log("Updated line number: " + updatedLineNumber);
+
+                              var updatedLearningPathFileContent = learningPathFileContentStr.substring(0, endIndex - lineNumber.length) + updatedLineNumber + learningPathFileContentStr.substring(endIndex, learningPathFileContentStr.length);
+
+                              console.log("Before" + learningPathFileContentStr);
+                              console.log("After" + updatedLearningPathFileContent);
+
+                              fs.writeFile(learningPathsDirectory + "/" + learningPathFile, updatedLearningPathFileContent, (err) => {
+                                if (err)
+                                {
+                                  console.log("Failed to write: " + err);
+                                }
+          
+                              });
+
+                            }
+                            else
+                            {
+                              UpdateManuallyReview(trimmedFilePath);
+                            }
+                          }
+                        }
+
                       }
                     });
-      
+
                   }
+                });
 
-
-                }
-
-
-                
+              }
+              else
+              {
+                // just check that the file still exists
+                fs.readFile(mergePathPrefix + pathsToCheck[pathIndex], (err, content) => {
+                  if (err || content === null || content.length === 0)
+                  {
+                    UpdateManuallyReview(trimmedFilePath);
+                  }
+                  else
+                  {
+                    console.log("Content that we read: " + content.toString());
+                  }
+                });
+  
               }
 
-              resolve()
 
-            });
-          });
+            }
 
-      
-          //core.setOutput('modifiedFiles', modifiedFiles);
-          //core.setOutput('manuallyReview', manuallyReview);
+
+            
+          }
+
+          //core.setOutput('modifiedFiles', modifiedFiles.join("\n"));
+          //core.setOutput('manuallyReview', manuallyReview.join("\n"));
         });
       });
-    //}
 
-    //core.setOutput('modifiedFiles', modifiedFiles.join(","));
-    //core.setOutput('manuallyReview', manuallyReview.join(","));
+  
+      //core.setOutput('modifiedFiles', modifiedFiles);
+      //core.setOutput('manuallyReview', manuallyReview);
+    });
 
   } catch (error) {
     core.setFailed(error.message);
   }
-
-  /*readFileAsync().then((modifiedFiles, manuallyReview) => {
-    core.setOutput('modifiedFiles', modifiedFiles.join(","));
-    core.setOutput('manuallyReview', manuallyReview.join(","));
-
-  });*/
 }
 
 // Call the main function to run the action
-//main();
-
-main().then(() => {
-  core.setOutput('modifiedFiles', modifiedFiles.join(","));
-  core.setOutput('manuallyReview', manuallyReview.join(","));
-
-});
+main();
