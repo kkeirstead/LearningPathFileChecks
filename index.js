d831e15e035fdc493046c17ca051dc773a57b810
kkeirstead <kkeirstead@microsoft.com>
2023-10-02 12:57:23 -0700

    Debugging.

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function UpdateManuallyReview(path, learningPathFile, lineNumber = -1)
 // This is currently primitive - can make it better as-needed.
 function CheckForEndOfLink(str, startIndex)
 {
-  return str.substr().indexOf(")"); // temporary
+  return str.substr(startIndex).indexOf(")"); // temporary
   /*
   const illegalRegex = /^[^()\[\]{} ,]+$/ // not accounting for periods at end
 
